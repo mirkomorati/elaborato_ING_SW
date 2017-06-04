249e2a77124cc1d22aa70d8da64de87423a15c7d
NoeMurr <noe.murr@outlook.com>
2017-06-04 14:18:23 +0200

    creata interfaccia per la serializzazione nel database

--- code/hdr/interfaces/ISerializable.hpp
+++ code/hdr/interfaces/ISerializable.hpp
@@ -7,7 +7,44 @@
 #ifndef _ISERIALIZABLE_H
 #define _ISERIALIZABLE_H
 
-class ISerializable {
-};
+#include <string>
+#include <map>
 
+namespace mm {
+  enum StoredTypes{
+    INTEGER,  // mapped to an int
+    REAL,     // mapped to a double
+    TEXT      // mapped to a std::string
+  };
+
+  union SerializedUnion{
+    std::string text;
+    int integer;
+    double real;
+  };
+
+  struct Serialized{
+  private:
+    SerializedUnion data;
+    StoredTypes type;
+  public:
+    StoredTypes getType() const noexcept;
+    bool isType(StoredTypes type) const noexcept;
+    const SerializedUnion &get() const noexcept;
+
+    Serialized(StoredTypes t, SerializedUnion data) noexcept;
+    ~Serialized() noexcept;
+  };
+
+  class ISerializable {
+  public:
+
+    virtual std::map<std::string, Serialized>
+    serialize() = 0;
+
+    virtual void
+    unserialize(std::map<std::string, Serialized>) = 0;
+
+  };
+}
 #endif //_ISERIALIZABLE_H
--- code/src/interfaces/ISerializable.cpp
+++ code/src/interfaces/ISerializable.cpp
@@ -9,3 +9,26 @@
 /**
  * ISerializable implementation
  */
+bool mm::Serialized::isType(mm::StoredTypes type) const {
+  return type == this->type;
+}
+
+const mm::SerializedUnion &mm::Serialized::get() const {
+  return data;
+}
+
+mm::StoredTypes mm::Serialized::getType() const {
+  return type;
+}
+
+mm::Serialized::Serialized(mm::StoredTypes t, mm::SerializedUnion data)
+    : type(t), data(data) {}
+
+mm::Serialized::~Serialized() {
+  if (type == StoredTypes::TEXT){
+    // su overflow è (&data.text)->string::~string()
+    data.text.string::~string();
+  }
+}
+
+
