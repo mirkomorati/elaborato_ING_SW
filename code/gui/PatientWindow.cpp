57c31f3968dc220d2624750e0972a5ed818aa16e
mirko <mirkomorati@gmail.com>
2017-12-31 10:54:35 +0100

    Adesso cambia anche il bottone

--- code/gui/PatientWindow.cpp
+++ code/gui/PatientWindow.cpp
@@ -617,12 +617,12 @@ void mm::PatientWindow::sortByPrescriptionHandler() {
     prescriptionList->unset_sort_func();
 
     // non so perché il set_icon_name non funziona come dovrebbe
-    if (button->get_icon_name() == "view-sort-ascending") {
-        button->set_icon_name("view-sort-descending");
+    if (button->get_stock_id() == "gtk-sort-descending") {
+        button->set_stock_id(Gtk::StockID("gtk-sort-ascending"));
         prescriptionList->set_sort_func(sigc::bind(sigc::mem_fun(this, &mm::PatientWindow::sortPrescriptionList),
                                                    Gtk::SortType::SORT_DESCENDING));
     } else {
-        button->set_icon_name("view-sort-ascending");
+        button->set_stock_id(Gtk::StockID("gtk-sort-descending"));
         prescriptionList->set_sort_func(sigc::bind(sigc::mem_fun(this, &mm::PatientWindow::sortPrescriptionList),
                                                    Gtk::SortType::SORT_ASCENDING));
     }
